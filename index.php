f12de5acba34a2a962cb0605fa895c7235cbea34
Yuanjing Xu <yuanjing.xu@gmail.com>
2016-02-18 13:34:35 -0600

    4. Testing jQuery https://thenewboston.com/videos.php?cat=32&video=18056

---
+++ index.php
@@ -0,0 +1,10 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>jQuery Example</title>
+</head>
+<body>
+    <p onclick="$(this).hide();">Paragraph</p>
+    <script type="text/javascript" src="js/jquery.js"></script>
+</body>
